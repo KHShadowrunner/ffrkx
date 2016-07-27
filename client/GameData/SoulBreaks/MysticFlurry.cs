7763ebee49b3b28c6745864c99db9c0f6c0d5844
Chris Moore <moorecp@gmail.com>
2016-07-27 07:58:50 -0400

    Added Seifer's event RM and SBs.

---
+++ client/GameData/SoulBreaks/MysticFlurry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFRKInspector.GameData.SoulBreaks
+{
+    class MysticFlurry : FFRKInspector.GameData.SoulBreak
+    {
+        public override uint SoulBreakId { get { return 20350006; } }
+        public override SchemaConstants.BuddyID BuddyId { get { return SchemaConstants.BuddyID.SQUALL; } }
+        public override SchemaConstants.ElementID Element { get { return SchemaConstants.ElementID.Ice; } }
+        public override SchemaConstants.Formulas Formula { get { return SchemaConstants.Formulas.Physical; } }
+        public override double Multiplier { get { return 1.02; } }
+        public override string Name { get { return "Mystic Flurry"; } }
+        public override int NumberOfHits { get { return 5; } }
+    }
+}
---
+++ client/GameData/SoulBreaks/ZantetsukenReverse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFRKInspector.GameData.SoulBreaks
+{
+    class ZantetsukenReverse : FFRKInspector.GameData.SoulBreak
+    {
+        public override uint SoulBreakId { get { return 20820004; } }
+        public override SchemaConstants.BuddyID BuddyId { get { return SchemaConstants.BuddyID.SEIFER; } }
+        public override SchemaConstants.Formulas Formula { get { return SchemaConstants.Formulas.Physical; } }
+        public override double Multiplier { get { return 2.00; } }
+        public override string Name { get { return "Zantetsuken Reverse"; } }
+        public override int NumberOfHits { get { return 3; } }
+    }
+}
